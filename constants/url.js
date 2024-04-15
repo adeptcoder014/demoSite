607317d2672a05a3769fcf4fdb4da0fe963aa1d8
Nischal Gupta <nischal@onecorp.co.in>
2024-04-15 16:03:20 +0530

    configuring deployed server

--- constants/url.js
+++ constants/url.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 
-const baseURL =  "http://localhost:8000"
+// const baseURL =  "http://localhost:8000"
+const baseURL =  "https://demoserver-3bmw.onrender.com"
 export default baseURL;
